842b7dc4d2efa2a3149fa641927acaab37d2a367
Kamerlord <kamerlord@gmail.com>
2018-11-14 12:54:11 +0100

    ShowHideUpgradePanel
    Added function to show and hide panel containing upgrades (in future)

---
+++ DevCompIdle/Assets/Scripts/UpgradePanelShowHide.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradePanelShowHide : MonoBehaviour {
+
+    public GameObject Panel;
+    int counter = 0;
+
+    
+
+    public void showhidePanel()
+    {
+        if (counter%2==0)
+        {
+            Panel.SetActive(true);
+            counter = 1;
+        } else
+        {
+            Panel.gameObject.SetActive(false);
+            counter = 0;
+        }
+    } 
+} 
